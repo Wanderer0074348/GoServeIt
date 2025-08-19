89c7ba6db272b6c2a10df77a1ea6bb2ee41f75dd
Tanay <tanaymatta27@gmail.com>
2025-08-19 19:35:31 +0400

    some wierd cleanup

--- go-dbstuff/pkg/models/book.go
+++ go-dbstuff/pkg/models/book.go
@@ -4,17 +4,9 @@ import (
 	"gorm.io/gorm"
 )
 
-// var db *gorm.DB
-
 type Book struct {
 	gorm.Model
 	Name        string `gorm:"" json:"name"`
 	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
-
-// func init() {
-// 	config.ConnectDb()
-// 	db = config.GetDB()
-// 	db.AutoMigrate(&Book{})
-// }
